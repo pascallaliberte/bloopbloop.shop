5dd392c046424d5ad45ea70df1556d3dc7b75be5
Pascal Laliberté <pascal@pascallaliberte.me>
2018-11-13 14:10:50 -0500

    Clean up scripts

--- assets/javascript/scripts.js
+++ assets/javascript/scripts.js
@@ -1,47 +1,13 @@
+/*
+  * reframe.js - Reframe.js: responsive iframes for embedded content
+  * @version v2.1.12
+  * @link https://github.com/dollarshaveclub/reframe.js#readme
+  * @author Jeff Wainwright <jjwainwright2@gmail.com> (http://jeffry.in)
+  * @license MIT
+*/
+!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):e.reframe=t()}(this,function(){"use strict";return function(e,t){var i="string"==typeof e?document.querySelectorAll(e):e,n=t||"js-reframe";"length"in i||(i=[i]);for(var o=0;o<i.length;o+=1){var r=i[o];if(-1!==r.className.split(" ").indexOf(n))return;var f=r.offsetHeight/r.offsetWidth*100,d=document.createElement("div");d.className=n;var a=d.style;a.position="relative",a.width="100%",a.paddingTop=f+"%";var s=r.style;s.position="absolute",s.width="100%",s.height="100%",s.left="0",s.top="0",r.parentNode.insertBefore(d,r),r.parentNode.removeChild(r),d.appendChild(r)}}});
+
 (function(){
-  if (typeof ScrollReveal == "undefined") {
-    return;
-  }
-
-  setSectionInView = function() {
-    var elInView = document.querySelector('[data-sr-revealed="true"]');
-    if (!elInView) {
-      return;
-    }
-
-    var sectionInView = elInView.getAttribute('id');
-    var htmlTag = document.querySelector('html');
-
-    if (!sectionInView) {
-      return;
-    }
-    htmlTag.setAttribute('data-section-in-view', sectionInView);
-  }
-
-  afterReveal = function(elScrolledTo) {
-    elScrolledTo.setAttribute('data-sr-revealed', true);
-    setSectionInView();
-  }
-
-  afterReset = function(elScrolledTo) {
-    elScrolledTo.setAttribute('data-sr-revealed', false);
-    setSectionInView();
-  }
-
-  addScrollReveal = function() {
-    var sr = ScrollReveal({
-      scale: 0.95,
-      opacity: 0.1,
-      distance: "50px",
-      reset: true,
-      viewFactor: 0.3,
-      delay: 300,
-      afterReveal: afterReveal,
-      afterReset: afterReset
-    });
-    sr.reveal('.scroll-reveal');
-  }
-
   makeVideosResponsive = function() {
     if (typeof reframe == "undefined") {
       return;
@@ -49,7 +15,5 @@
 
     reframe('iframe');
   }
-
-  addScrollReveal();
   makeVideosResponsive();
 })()
